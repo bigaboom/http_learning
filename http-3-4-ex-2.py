773b22b91f49d91564bfdd63b648c913edf2f4ae
bigaboom <index2.php@gmail.com>
2019-08-13 19:30:34 +0300

    Задача с ссылками решена

--- http-3-4-ex-2.py
+++ http-3-4-ex-2.py
@@ -1,7 +1,7 @@
 import re
 import requests
 
-pattern = r"<a(?:[\w =\'\"&?.:\/,;\(\)\s]*)?\s?(?:href(?:\s)?=(?:\s)?(?:[\"\']?))(?:[a-zA-Z\-]*:\/\/)?([a-zA-Z]{1}[a-zA-Z0-9.-]*)(?:[\w]*\/\s)*(?:[\"\']?)(?:[\w =\'\"&?.:\/,;\(\)\s]*)?>"
+pattern = r"<a(?:[\w =\'\"&?.:\/,;\(\)\s\[\]\+]*)?\s?(?:href(?:\s)?=(?:\s)?(?:[\"\']?))(?:[a-zA-Z\-]*:\/\/)?([a-zA-Z]{1}[a-zA-Z0-9.-]*)(?:[\w]*\/\s)*(?:[\"\']?)(?:[\w =\[\]\+\'\"&?.:\/,;\(\)а-яА-Я\s-]*)?>"
 ended = []
 link = input()
 links_ended = []
@@ -11,12 +11,10 @@ links = re.findall(pattern, str(req.text))
 
 for link in links:
     #print(link)
-    if (link not in links_ended):
+    if (link not in links_ended) and (link not in ['..','.']) and (link[-5:] != '.html') and (link[-4:] != '.htm'):
         links_ended.append(link)
     #print(link[5])
 
 links_ended.sort()
 for i in links_ended:
     print(i)
-
-#http://pastebin.com/raw/2mie4QYa
